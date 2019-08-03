0f1def57b0bc978377ff60a0e7085905e85c03a5
Jiang Junyu <double.chiang@gmail.com>
2019-08-03 12:08:26 -0700

    finialize two sums

---
+++ two_sum.rb
@@ -0,0 +1,18 @@
+def two_sum(nums, target)
+    
+    base =0
+    while base < nums.length
+        oppo = base +1
+        while oppo < nums.length
+            if nums[base] + nums[oppo]  == target
+                return [base, oppo]
+            end
+            puts format("%d, %d", base, oppo)
+            oppo +=1
+        end
+        base +=1
+    end
+end
+
+p two_sum([2, 7, 11, 15], 9)
+
